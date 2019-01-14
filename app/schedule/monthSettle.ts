861d932f7721fa6b382a47cfbffbf76438fc7ec9
alixez <alixe.z@foxmail.com>
2019-01-14 10:15:19 +0800

    fix cron

--- app/schedule/monthSettle.ts
+++ app/schedule/monthSettle.ts
@@ -7,8 +7,8 @@ export default class MonthSettle extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
   static get schedule() {
     return {
-      // cron: '0 0 0 1 * *',
-      interval: '180s', // 1 分钟间隔
+      cron: '0 0 0 1 * *',
+      // interval: '180s', // 1 分钟间隔
       type: 'all',
     };
   }
